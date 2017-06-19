81b8f107904a5e89540637df12a1031ad667cbe0
cuongcau93 <cuongnm22@wru.vn>
2017-06-19 15:03:25 +0700

    4.2: Creating a Resource
    ver1

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        //[HttpGet("api/cities")]
+        //public JsonResult GetCities()
+        //{
+        //    return new JsonResult(new List<Object>()
+        //    {
+        //        new {id = 1, Name = "New York City"},
+        //        new {id = 2, Name = "Ha Noi City"}
+        //    });
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+
+    }
+}
